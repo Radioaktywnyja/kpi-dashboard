a3897a81102b29e78efb05345ef85be153d6b9be
Radioaktywnyja <milosz.kusak@gmail.com>
2021-03-03 21:58:33 +0100

    edit header

--- src/containers/TheHeader.vue
+++ src/containers/TheHeader.vue
@@ -1,16 +1,6 @@
 <template>
   <CHeader fixed with-subheader light>
-    <CToggler
-      in-header
-      class="ml-3 d-lg-none"
-      @click="$store.commit('toggleSidebarMobile')"
-    />
-    <CToggler
-      in-header
-      class="ml-3 d-md-down-none"
-      @click="$store.commit('toggleSidebarDesktop')"
-    />
-    <CHeaderBrand class="mx-auto d-lg-none" to="/">
+    <CHeaderBrand class="ml-4 mr-auto mr-lg-4" to="/">
       <CIcon name="logo" height="48" alt="Logo"/>
     </CHeaderBrand>
     <CHeaderNav class="d-md-down-none mr-auto">
@@ -24,33 +14,10 @@
           Users
         </CHeaderNavLink>
       </CHeaderNavItem>
-      <CHeaderNavItem class="px-3">
-        <CHeaderNavLink>
-          Settings
-        </CHeaderNavLink>
-      </CHeaderNavItem>
     </CHeaderNav>
     <CHeaderNav class="mr-4">
-      <CHeaderNavItem class="d-md-down-none mx-2">
-        <CHeaderNavLink>
-          <CIcon name="cil-bell"/>
-        </CHeaderNavLink>
-      </CHeaderNavItem>
-      <CHeaderNavItem class="d-md-down-none mx-2">
-        <CHeaderNavLink>
-          <CIcon name="cil-list"/>
-        </CHeaderNavLink>
-      </CHeaderNavItem>
-      <CHeaderNavItem class="d-md-down-none mx-2">
-        <CHeaderNavLink>
-          <CIcon name="cil-envelope-open"/>
-        </CHeaderNavLink>
-      </CHeaderNavItem>
       <TheHeaderDropdownAccnt/>
     </CHeaderNav>
-    <CSubheader class="px-3">
-      <CBreadcrumbRouter class="border-0 mb-0"/>
-    </CSubheader>
   </CHeader>
 </template>
 
